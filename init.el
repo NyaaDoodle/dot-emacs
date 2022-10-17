4fdbc30b3712f1334543b47fcba8d24eba14833e
NyaaDoodle <msverdlov4@gmail.com>
2022-10-17 19:37:18 +0300

    Add files via upload

--- init.el
+++ init.el
@@ -5,19 +5,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wombat))
  '(ispell-dictionary nil)
- '(package-selected-packages '(counsel swiper doom-modeline ivy use-package))
+ '(package-selected-packages
+   '(all-the-icons counsel swiper doom-modeline ivy use-package))
  '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack"
-				:foundry "SRC"
-		  		:slant normal
-		   		:weight normal
-				:height 241
-				:width normal)))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 241 :width normal)))))
 
 (setq inhibit-startup-message t)    ;; Disable landing page on startup
 (setq visible-bell t)               ;; Enable visual bell, replacing audible bell
@@ -28,6 +24,14 @@
 (set-fringe-mode 10)                ;; Give some breathing room
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -66,3 +70,24 @@
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
