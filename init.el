aa0fe57c69a263acbca3a14ef5a57592bf87d53d
NyaaDoodle <msverdlov4@gmail.com>
2022-10-17 23:03:31 +0300

    Add files via upload

--- init.el
+++ init.el
@@ -1,93 +1,58 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wombat))
- '(ispell-dictionary nil)
- '(package-selected-packages
-   '(all-the-icons counsel swiper doom-modeline ivy use-package))
- '(tab-width 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 241 :width normal)))))
-
-(setq inhibit-startup-message t)    ;; Disable landing page on startup
-(setq visible-bell t)               ;; Enable visual bell, replacing audible bell
-(scroll-bar-mode -1)                ;; Disable visible scrollbar
-(tool-bar-mode -1)                  ;; Disable the toolbar
-(tooltip-mode -1)                   ;; Disable the tooltips
-(menu-bar-mode -1)                  ;; Disable the menu bar
-(set-fringe-mode 10)                ;; Give some breathing room
-
+;; Global settings
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(set-fringe-mode 10)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+;; Font
+(set-face-attribute 'default nil :font "Hack" :height 220)
 
+;; Package management
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-						 ("org" . "https://orgmode.org/elpa/")
-						 ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
 (use-package ivy
-    :diminish
-    :bind (("C-s" . swiper)
-    :map ivy-minibuffer-map
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
          ("TAB" . ivy-alt-done)	
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
-    :map ivy-switch-buffer-map
+         :map ivy-switch-buffer-map
          ("C-k" . ivy-previous-line)
          ("C-l" . ivy-done)
          ("C-d" . ivy-switch-buffer-kill)
-    :map ivy-reverse-i-search-map
+         :map ivy-reverse-i-search-map
          ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill)))
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config)
 (ivy-mode 1)
-(use-package swiper)
-(use-package counsel)
+
+(use-package all-the-icons)
 
 (use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-(use-package all-the-icons
-  :ensure t)
+    :init (doom-modeline-mode 1))
 
 (use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+    :config
+    (setq doom-themes-enable-bold t
+	  doom-themes-enable-italic t)
+    (load-theme 'doom-monokai-pro t))
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+(custom-set-variables
+ '(package-selected-packages
+   '(use-package doom-themes doom-modeline counsel all-the-icons)))
+(custom-set-faces)
